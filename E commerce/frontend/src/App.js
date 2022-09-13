eff9c0f0c5513503352e15d9cb42571ac0fa71c9
Alankar Jamle <77237523+Alankar99@users.noreply.github.com>
2022-09-14 00:25:54 +0530

    Add files via upload

---
+++ E commerce/frontend/src/App.js
@@ -0,0 +1,257 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Badge from 'react-bootstrap/Badge'
+import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
+import { useContext, useEffect, useState } from 'react'
+import { Store } from './Store'
+import CartScreen from './Screens/CartScreen'
+import SigninScreen from './Screens/SigninScreen'
+import SignupScreen from './Screens/SignupScreen'
+import ShippingAddressScreen from './Screens/ShippingAddressScreen'
+import PaymentMethodScreen from './Screens/PaymentMethodScreen'
+import PlaceOrderScreen from './Screens/PlaceOrderScreen'
+import OrderScreen from './Screens/OrderScreen'
+import OrderHistoryScreen from './Screens/OrderHistoryScreen'
+import ProfileScreen from './Screens/ProfileScreen'
+import { Button } from 'react-bootstrap'
+import { getError } from './utils'
+import axios from 'axios'
+import SearchBox from './components/SearchBox'
+import SearchScreen from './Screens/SearchScreen'
+import ProtectedRoute from './components/ProtectedRoute'
+import DashboardScreen from './Screens/DashboardScreen'
+import AdminRoute from './components/AdminRoute'
+import ProductListScreen from '../src/Screens/ProductListScreen'
+import ProductEditScreen from './Screens/ProductEditScreen'
+import OrderListScreen from './Screens/OrderListScreen'
+import UserListScreen from './Screens/UserListScreen'
+import UserEditScreen from './Screens/UserEditScreen'
+function App() {
+  const { state, dispatch: ctxDispatch } = useContext(Store)
+  const { cart, userInfo } = state
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' })
+    localStorage.removeItem('userInfo')
+    localStorage.removeItem('shippingAddress')
+    localStorage.removeItem('paymentMethod')
+    window.location.href = '/signin'
+  }
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false)
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`)
+        setCategories(data)
+      } catch (err) {
+        toast.error(getError(err))
+      }
+    }
+    fetchCategories()
+  }, [])
+  return (
+    <BrowserRouter>
+      <div
+        className={
+          sidebarIsOpen
+            ? 'd-flex flex-column site-container active-cont'
+            : 'd-flex flex-column site-container'
+        }
+      >
+        <ToastContainer position="bottom-center" limit={1} />
+        <header>
+          <Navbar className="nav-top" expand="lg">
+            <Container>
+              <Button onClick={() => setSidebarIsOpen(!sidebarIsOpen)}>
+                <i className="fas fa-bars"></i>
+              </Button>
+              <LinkContainer to="/">
+                <Navbar.Brand className="nav-text">
+                  {' '}
+                  <i className="fas fa-shopping-cart"></i> Shopify
+                </Navbar.Brand>
+              </LinkContainer>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <SearchBox />
+                <Nav className="me-auto w-100  justify-content-end">
+                  <Link to="/cart" className="nav-link nav-text">
+                    Cart
+                    {cart.cartItems.length > 0 && (
+                      <Badge pill bg="danger">
+                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                      </Badge>
+                    )}
+                  </Link>
+                  {userInfo ? (
+                    <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                      <LinkContainer to="/profile">
+                        <NavDropdown.Item>User Profile</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/orderhistory">
+                        <NavDropdown.Item>Order History</NavDropdown.Item>
+                      </LinkContainer>
+                      <NavDropdown.Divider />
+                      <Link
+                        className="dropdown-item"
+                        to="#signout"
+                        onClick={signoutHandler}
+                      >
+                        Sign Out
+                      </Link>
+                    </NavDropdown>
+                  ) : (
+                    <Link className="nav-link nav-text" to="/signin">
+                      Sign In
+                    </Link>
+                  )}
+                  {userInfo && userInfo.isAdmin && (
+                    <NavDropdown title="Admin" id="admin-nav-dropdown">
+                      <LinkContainer to="/admin/dashboard">
+                        <NavDropdown.Item>Dashboard</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/admin/products">
+                        <NavDropdown.Item>Products</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/admin/orders">
+                        <NavDropdown.Item>Orders</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/admin/users">
+                        <NavDropdown.Item>Users</NavDropdown.Item>
+                      </LinkContainer>
+                    </NavDropdown>
+                  )}
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </header>
+        <div
+          className={
+            sidebarIsOpen
+              ? 'active-nav side-navbar d-flex justify-content-between flex-wrap flex-column'
+              : 'side-navbar d-flex justify-content-between flex-wrap flex-column'
+          }
+        >
+          <Nav className="flex-column text-white w-100 p-2">
+            <Nav.Item>
+              <strong className="side-top">Categories</strong>
+            </Nav.Item>
+            {categories.map((category) => (
+              <Nav.Item key={category}>
+                <LinkContainer
+                  to={`/search?category=${category}`}
+                  onClick={() => setSidebarIsOpen(false)}
+                >
+                  <Nav.Link className="side-nav-item">{category}</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </div>
+        <main>
+          <Container className="mt-5">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <ProfileScreen />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route path="/shipping" element={<ShippingAddressScreen />} />
+              <Route path="/payment" element={<PaymentMethodScreen />} />
+              <Route
+                path="/admin/dashboard"
+                element={
+                  <AdminRoute>
+                    <DashboardScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/products"
+                element={
+                  <AdminRoute>
+                    <ProductListScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/order/:id"
+                element={
+                  <ProtectedRoute>
+                    <OrderScreen />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/product/:id"
+                element={
+                  <AdminRoute>
+                    <ProductEditScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/orders"
+                element={
+                  <AdminRoute>
+                    <OrderListScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/users"
+                element={
+                  <AdminRoute>
+                    <UserListScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/user/:id"
+                element={
+                  <AdminRoute>
+                    <UserEditScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route path="/search" element={<SearchScreen />} />
+              <Route
+                path="/orderhistory"
+                element={
+                  <ProtectedRoute>
+                    <OrderHistoryScreen />
+                  </ProtectedRoute>
+                }
+              ></Route>
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">
+            © 2022 Shopify. All Rights Reserved.
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ Twitter-clone/README.md
@@ -0,0 +1,73 @@
+# This is my first Nagarro Project
+### Here is live demo: https://twitter-cln.herokuapp.com
+## Todos:
+- [ ] Frontend
+    - [x] TwitterButton component
+    - [x] Welcome page
+    - [x] Register popup
+    - [x] Login page
+    - [x] Responsive Welcome page
+    - [x] Sidebar
+    - [x] Responsive Sidebar
+    - [x] Right container
+    - [x] Components for right container
+    - [x] All components responsive
+    - [x] Fixed headers for pages
+    - [x] Profile page
+    - [x] Bookmarks page
+    - [ ] Notifications page
+    - [x] Child pages menu for some parent pages
+    - [x] Tweet component
+    - [x] Profile component for sidebar
+    - [x] Add tweet component
+    - [x] Toggle action list for tweet component
+    - [x] Add tweet popup
+    - [x] Action button component
+    - [x] Image zoomer
+    - [ ] Folowers list
+    - [ ] Tweet likers list
+    - [x] Tweets detail page
+    - [x] Tweet replies
+    - [ ] Nested tweets(retweet with comment)
+    - [ ] Retweet
+    - [ ] Tweet pinning
+    - [ ] Search
+    - [ ] Shortcuts
+    - [x] Delete tweet
+    - [x] Show parent tweet in tweet details page
+    - [x] Image preview on tweet adding
+    - [ ] Routing
+        - [x] Route guards
+        - [ ] Define all routes
+        - [ ] Define all child routes
+    - [x] Update profile popup
+    - [x] Backend Integration
+    - [ ] Uploading contents piece by piece.
+    - [ ] Upload new content at the end of the page
+    - [ ] Loading spinner
+    - [ ] Copy link of tweet
+- [ ] Backend
+    - [ ] Install modules
+        - [x] express
+        - [x] mongoose
+        - [x] cors
+        - [x] nodemon
+        - [x] body-parser
+        - [ ] socketio
+    - [x] Express configurations
+    - [x] Mongoose configurations
+        - [x] Create models
+            - [x] User model
+            - [x] Tweet model
+        - [x] Relationships
+    - [x] Api
+    - [ ] Notifications for users
+    - [ ] Useful functions for apis
+        - [ ] Follow suggestion
+    - [ ] Uploading contents piece by piece
+    - [ ] Upload new content at the end of the page
+- [ ] Socket support for realtime
+- [x] Add images to readme
+## Specifications:
+* Responsive.
+* Its offering many functions of twitter.
